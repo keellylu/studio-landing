e4caa19ee0c6606fe861442ebda6e2099c7d63fa
Kelly Lu <95010893+keellylu@users.noreply.github.com>
2025-02-07 14:03:54 -0800

    changed layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,18 +17,18 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <title>Overeasy</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🥚</text></svg>"></link>
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
-        <meta name="title" content="Overeasy: Your Computer Vision Copilot" />
-        <meta name="description" content="Overeasy is creating a Computer Vision Copilot that beats Gemini and GPT-4V on vision tasks, making computer vision accessible and easy for developers." />
+        <meta name="title" content="Talk, Record, Earn." />
+        <meta name="description" content="At Overeasy, we're seeking native speakers from around the world for flexible, part‐time roles with competitive compensation." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://overeasy.sh/" />
-        <meta property="og:title" content="Overeasy: Your Computer Vision Copilot" />
-        <meta property="og:description" content="Overeasy is creating a Computer Vision Copilot that beats Gemini and GPT-4V on vision tasks, making computer vision accessible and easy for developers." />
-        <meta property="og:image" content="https://overeasy.sh/meta-image.png" />
+        <meta property="og:title" content="Talk, Record, Earn." />
+        <meta property="og:description" content="At Overeasy, we're seeking native speakers from around the world for flexible, part‐time roles with competitive compensation." />
+        <meta property="og:image" content="" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://overeasy.sh/" />
-        <meta property="twitter:title" content="Overeasy: Your Computer Vision Copilot" />
-        <meta property="twitter:description" content="Overeasy is creating a Computer Vision Copilot that beats Gemini and GPT-4V on vision tasks, making computer vision accessible and easy for developers." />
-        <meta property="twitter:image" content="https://overeasy.sh/meta-image.png" />
+        <meta property="twitter:title" content="Talk, Record, Earn." />
+        <meta property="twitter:description" content="At Overeasy, we're seeking native speakers from around the world for flexible, part‐time roles with competitive compensation." />
+        <meta property="twitter:image" content="" />
 
       </head>
 
